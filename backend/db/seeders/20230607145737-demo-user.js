2ca2ac98221678c589eef9c1282434026fc9a9cf
David Soto <ds051128@gmail.com>
2023-06-07 18:36:35 -0500

    updated seeder file for user data

--- backend/db/seeders/20230607145737-demo-user.js
+++ backend/db/seeders/20230607145737-demo-user.js
@@ -13,16 +13,22 @@ module.exports = {
 			options,
 			[
 				{
+					firstname: "David",
+					lastname: "Soto",
 					email: "demo@user.io",
 					username: "Demo-lition",
 					hashedPassword: bcrypt.hashSync("password"),
 				},
 				{
+					firstname: "Robert",
+					lastname: "Barquin",
 					email: "user1@user.io",
 					username: "FakeUser1",
 					hashedPassword: bcrypt.hashSync("password2"),
 				},
 				{
+					firstname: "Silver",
+					lastname: "Surfer",
 					email: "user2@user.io",
 					username: "FakeUser2",
 					hashedPassword: bcrypt.hashSync("password3"),
