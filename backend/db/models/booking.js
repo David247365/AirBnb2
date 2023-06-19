efd20f7be71c3d156889c113873fcad0222514e1
David Soto <ds051128@gmail.com>
2023-06-19 10:01:35 -0500

    updated models and migrations

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -28,6 +28,10 @@ module.exports = (sequelize, DataTypes) => {
 			userId: {
 				type: DataTypes.INTEGER,
 				allowNull: false,
+				references: {
+					model: "Users",
+					key: "id",
+				},
 			},
 			startDate: {
 				type: DataTypes.DATE,
