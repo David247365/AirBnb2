050657da156e6766e21e20a488a903baea375f2a
David Soto <ds051128@gmail.com>
2023-06-19 10:16:03 -0500

    updated models and migrations

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
 			Spot.belongsTo(models.User, { foreignKey: "userId " });
 			Spot.hasMany(models.SpotImage, { foreignKey: "spotId" });
 			Spot.hasMany(models.Review, { foreignKey: "spotId" });
+			Spot.hasMany(models.Booking, { foreignKey: "spotId" });
 		}
 	}
 	Spot.init(
