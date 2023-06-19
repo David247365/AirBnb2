c0a74b32e33c93dba5b41e6e9b0cc9368052923b
David Soto <ds051128@gmail.com>
2023-06-19 10:11:00 -0500

    updated models and migrations

--- backend/db/migrations/20230619130604-create-review.js
+++ backend/db/migrations/20230619130604-create-review.js
@@ -9,50 +9,54 @@ if (process.env.NODE_ENV === "production") {
 module.exports = {
 	async up(queryInterface, Sequelize) {
 		options.tableName = "Reviews";
-		await queryInterface.createTable("Reviews", {
-			id: {
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-				type: Sequelize.INTEGER,
-			},
-			userId: {
-				type: Sequelize.INTEGER,
-				references: {
-					model: "Users",
-					key: "id",
+		await queryInterface.createTable(
+			"Reviews",
+			{
+				id: {
+					allowNull: false,
+					autoIncrement: true,
+					primaryKey: true,
+					type: Sequelize.INTEGER,
+				},
+				userId: {
+					type: Sequelize.INTEGER,
+					references: {
+						model: "Users",
+						key: "id",
+					},
+				},
+				spotId: {
+					type: Sequelize.INTEGER,
+					references: {
+						model: "Spots",
+						key: "id",
+					},
+				},
+				review: {
+					type: Sequelize.STRING,
+				},
+				stars: {
+					type: Sequelize.INTEGER,
+				},
+				createdAt: {
+					type: Sequelize.DATE,
+				},
+				updatedAt: {
+					type: Sequelize.DATE,
+				},
+				createdAt: {
+					allowNull: false,
+					type: Sequelize.DATE,
+					defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+				},
+				updatedAt: {
+					allowNull: false,
+					type: Sequelize.DATE,
+					defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
 				},
 			},
-			spotId: {
-				type: Sequelize.INTEGER,
-				references: {
-					model: "Spots",
-					key: "id",
-				},
-			},
-			review: {
-				type: Sequelize.STRING,
-			},
-			stars: {
-				type: Sequelize.INTEGER,
-			},
-			createdAt: {
-				type: Sequelize.DATE,
-			},
-			updatedAt: {
-				type: Sequelize.DATE,
-			},
-			createdAt: {
-				allowNull: false,
-				type: Sequelize.DATE,
-				defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-			},
-			updatedAt: {
-				allowNull: false,
-				type: Sequelize.DATE,
-				defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-			},
-		});
+			options
+		);
 	},
 	async down(queryInterface, Sequelize) {
 		options.tableName = "Reviews";
